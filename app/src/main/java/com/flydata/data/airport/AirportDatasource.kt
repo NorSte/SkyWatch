e0a14264d1627aa06156e018ff81b079ca25850b
eals01 <eastorks@uio.no>
2023-04-29 17:51:45 +0200

    implement cache for airports

--- app/src/main/java/com/flydata/data/airport/AirportDatasource.kt
+++ app/src/main/java/com/flydata/data/airport/AirportDatasource.kt
@@ -12,20 +12,40 @@ class AirportDatasource {
     // URL til Avinor-API
     private val flightsPath = "https://flydata.avinor.no/XmlFeed.asp?TimeFrom=1&TimeTo=2&airport="
 
+    // cache
+    private var airportCache: MutableList<Airport> = mutableListOf()
+
     suspend fun fetchAirportFlights(
         iata: String,
         typeOfListing: TypeOfListing
     ): MutableList<AirportFlight> {
+        // sjekk om flightDetails ligger i cache
+        val potentialAirport = airportCache.find { airport ->
+            airport.iata == iata
+        }
+        if (potentialAirport != null) {
+            var airportFlights = potentialAirport.airportFlights
+            airportFlights = airportFlights.filter {
+                if (typeOfListing == TypeOfListing.ARRIVAL) {
+                    it.arrDep == "A"
+                } else {
+                    it.arrDep == "D"
+                }
+            } as MutableList<AirportFlight>
+
+            println("Brukte cache")
+            return airportFlights
+        }
+
+        // gjør uthenting og hopper over de første to taggene for at parsingen skal fungere
         var data: String = client.get(flightsPath + iata).body()
-
-        // Må hoppe over de første to taggene for at parsingen skal fungere
         data = data.substring(data.indexOf('>') + 1, data.length - 1)
         data = data.substring(data.indexOf('>') + 1, data.length - 1)
 
+        // parser API-svar og filtrerer etter ankomst/avgang
         val inputStream: InputStream = data.byteInputStream()
-
-        var airportFlights = AirportFlightsXmlParser().parse(inputStream)
-        airportFlights = airportFlights.filter {
+        val airportFlights = AirportFlightsXmlParser().parse(inputStream)
+        val filteredAirportFlights = airportFlights.filter {
             if (typeOfListing == TypeOfListing.ARRIVAL) {
                 it.arrDep == "A"
             } else {
@@ -33,9 +53,16 @@ class AirportDatasource {
             }
         } as MutableList<AirportFlight>
 
-        return airportFlights
+        // legg til airport i cache og returner
+        airportCache.add(Airport(iata, airportFlights))
+        return filteredAirportFlights
     }
 }
+
+data class Airport(
+    val iata: String,
+    val airportFlights: MutableList<AirportFlight>
+)
 
 data class AirportFlight(
     val airline: String,
